fdd2645400923ade1faaa2086644c8ca7903259e
Jack Rigan <73765650+JackXN@users.noreply.github.com>
2021-09-21 17:52:56 -0600

    Imported bot.js and app.js

---
+++ app.js
@@ -0,0 +1,118 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {
+  client,
+  sendNewCoin,
+  sendNewCoinAlert,
+  sendNewCoinReminder,
+  sendPercent,
+  sendPeriodicPrice,
+  sendPrepump, 
+  sendPrepumpAlert,
+  sendPrepumpReminder,
+  sendRoutineCommands
+} = require('./bot');
+require('dotenv').config();
+
+const { DISCORD_KEY, WEBHOOK_URL } = process.env;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+client.login(DISCORD_KEY);
+
+/*
+
+  🟫crypto-alerts:
+    - 10%, 30 min
+  🟨sophomore-alerts:
+    - 10%, 15 min
+  🟩junior-alerts:
+    - 8%, 10 min
+    - 10%, 10 min
+    + prepump
+  🟦senior-alerts:
+    - 20%, 10 min
+    + prepump
+
+*/
+
+const getPercentFromMessage = (msg) => {
+  const regex = /[0-9]+(.[0-9])+/i;
+  const match = msg.match(regex)[0];
+  return match ? match : null;
+};
+
+const getMinutesFromWindow = (window) => {
+  const regex = /\d+/;
+  const match = window.match(regex)[0];
+  return match ? match : null;
+};
+
+
+app.post(`/${WEBHOOK_URL}`, (req, res) => {
+  const { type, currency, message, window, percent } = req.body;
+  if(currency && !(currency.includes("DOWN") || currency.includes("UP"))) {
+    switch(type) {
+      case "percent_price":
+        if(getMinutesFromWindow(window) === "30") {
+          // Freshman level
+          sendPercent(currency, message, getPercentFromMessage(message), "🟫crypto-alerts");
+          sendPercent(currency, message, getPercentFromMessage(message), "🟨sophomore-alerts");
+          sendPercent(currency, message, getPercentFromMessage(message), "🟩junior-alerts");
+          sendPercent(currency, message, getPercentFromMessage(message), "🟦senior-alerts");
+        } else if (getMinutesFromWindow(window) === "15") {
+          // Sophomore level
+          sendPercent(currency, message, getPercentFromMessage(message), "🟨sophomore-alerts");
+          sendPercent(currency, message, getPercentFromMessage(message), "🟩junior-alerts");
+          sendPercent(currency, message, getPercentFromMessage(message), "🟦senior-alerts");
+        } else if (getMinutesFromWindow(window) === "10") {
+          if(percent < 11) {
+            // Junior level
+            sendPercent(currency, message, getPercentFromMessage(message), "🟩junior-alerts");
+            sendPercent(currency, message, getPercentFromMessage(message), "🟦senior-alerts");
+          } else if(percent > 11) {
+            // Senior level
+            sendPercent(currency, message, getPercentFromMessage(message), "🟦senior-alerts");
+          }
+        } else if (getMinutesFromWindow(window) === "1") {
+          // Junior level
+          // Senior alert
+          sendPrepumpReminder("🟫crypto-alerts");
+          sendPrepumpReminder("🟨sophomore-alerts");
+          sendPrepump(currency, message, getPercentFromMessage(message), "🟩junior-alerts");
+          sendPrepumpAlert(currency, message, getPercentFromMessage(message), "🟦senior-alerts");
+        }
+        break;
+      case "new_coin":
+        // Sophomore level
+        sendNewCoinReminder("🟫crypto-alerts");
+        sendNewCoin(currency, message, "🟨sophomore-alerts");
+        sendNewCoin(currency, message, "🟩junior-alerts");
+        sendNewCoinAlert(currency, message, "🟦senior-alerts");
+        break;
+      case "periodic_price":
+        // Freshman level
+        sendPeriodicPrice(message, "🟫crypto-alerts");
+        sendPeriodicPrice(message, "🟨sophomore-alerts");
+        sendPeriodicPrice(message, "🟩junior-alerts");
+        sendPeriodicPrice(message, "🟦senior-alerts");
+        break;
+      default:
+        break;
+    }
+    return res.status(200).end();
+  }
+  return res.status(200).end();
+});
+
+app.get('/uptime', (req, res) => {
+  sendRoutineCommands('🛑sandbox🛑');
+  res.status(200).end();
+});
+
+module.exports = app;
---
+++ bot.js
@@ -0,0 +1,131 @@
+/* eslint-disable no-console */
+const Discord = require('discord.js');
+const Axios = require('axios');
+const sharp = require("sharp")
+require('dotenv').config();
+
+const { NOMICS_KEY } = process.env;
+
+const client = new Discord.Client();
+
+// Bot Methods
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// Notices
+
+
+
+
+const sendPercent = async (currency, msg, percent, group) => {
+  const logo = `https://icons.bitbot.tools/api/${currency}/64x64`;
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+    .setTitle(`+${percent}%`)
+    .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+    .setColor(0x00ff00)
+    .setDescription(msg);
+  channel.send(embed);
+};
+
+const sendPrepump = async (currency, msg, percent, group) => {
+  const logo = `https://icons.bitbot.tools/api/${currency}/64x64`;
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+    .setTitle(`POSSIBLE PRE-PUMP: +${percent}%`)
+    .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+    .setColor(0xff0000)
+    .setDescription(msg);
+  channel.send(embed);
+};
+
+const sendNewCoin = async (currency, msg, group) => {
+  const logo = `https://icons.bitbot.tools/api/${currency}/64x64`;
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Newly listed!")
+    .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+    .setColor(0xffff00)
+    .setFooter("Upgrade to Senior to receive a notification when this happens")
+    .setDescription(msg);
+  channel.send(embed);
+};
+
+const sendPeriodicPrice = (msg, group) => {
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+  .setTitle(msg)
+  .setColor(0xff9900);
+  channel.send(embed);
+};
+
+// Alerts
+
+const sendNewCoinAlert = async (currency, msg, group) => {
+  const logo = `https://icons.bitbot.tools/api/${currency}/64x64`;
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+    .setTitle("Newly listed!")
+    .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+    .setColor(0xffff00)
+    .setDescription(msg);
+  channel.send("@here New coin listing:");
+  channel.send(embed);
+};
+
+const sendPrepumpAlert = async (currency, msg, percent, group) => {
+  const logo = `https://icons.bitbot.tools/api/${currency}/64x64`;
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+    .setTitle(`POSSIBLE PRE-PUMP: +${percent}%`)
+    .setAuthor(currency, logo, `https://www.binance.com/en/trade/${currency}_BTC`)
+    .setColor(0xff0000)
+    .setDescription(msg);
+  channel.send("@here Possible pre-pump detected:");
+  channel.send(embed);
+};
+
+// Upgrade Reminders
+
+const sendNewCoinReminder = async (group) => {
+  const logo = `https://image.flaticon.com/icons/png/512/179/179386.png`;
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+    .setTitle("New coin listing detected!")
+    .setAuthor("Hidden alert", logo)
+    .setColor(0xff5757)
+    .setDescription("Upgrade to Sophomore to see new coin listing.");
+  channel.send(embed);
+};
+
+const sendPrepumpReminder = async (group) => {
+  const logo = `https://image.flaticon.com/icons/png/512/179/179386.png`;
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  const embed = new Discord.MessageEmbed()
+    .setTitle("New possible pre-pump detected!")
+    .setAuthor("Hidden alert", logo)
+    .setColor(0xff5757)
+    .setDescription("Upgrade to Junior to see the pre-pumped coin.");
+  channel.send(embed);
+};
+
+const sendRoutineCommands = async (group) => {
+  const channel = client.channels.cache.find((chnl) => chnl.name === group);
+  channel.send('!d bump');
+  channel.send('!refreshRanks all');
+};
+
+module.exports = {
+  client,
+  sendNewCoin,
+  sendNewCoinAlert,
+  sendNewCoinReminder,
+  sendPercent,
+  sendPeriodicPrice,
+  sendPrepump,
+  sendPrepumpAlert,
+  sendPrepumpReminder,
+  sendRoutineCommands
+};
